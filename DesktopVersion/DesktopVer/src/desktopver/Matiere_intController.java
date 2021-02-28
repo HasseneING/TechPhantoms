3db79bd690647cb8437ce16bb330b4e5efd79bd2
amine <mohamedamine.arbaoui@esprit.tn>
2021-02-28 14:55:37 +0100

    change name

--- DesktopVersion/DesktopVer/src/desktopver/Matiere_intController.java
+++ DesktopVersion/DesktopVer/src/desktopver/Matiere_intController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package desktopver;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +14,7 @@ import javafx.fxml.Initializable;
  *
  * @author PC
  */
-public class FXMLController implements Initializable {
+public class Matiere_intController implements Initializable {
 
     /**
      * Initializes the controller class.
