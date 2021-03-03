ba91ce99360a2f5bb0e50c37dc42f42929099bbe
Hassene <mohamedhassene.hamed@esprit.tn>
2021-03-03 20:48:55 +0100

    Commit

--- DesktopVersion/DesktopVer/src/controller/AvailabilityScheduleController.java
+++ DesktopVersion/DesktopVer/src/controller/AvailabilityScheduleController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package desktopver;
+package controller;
 
 import java.net.URL;
 import java.time.LocalDate;
--- DesktopVersion/DesktopVer/src/controller/Matiere_intController.java
+++ DesktopVersion/DesktopVer/src/controller/Matiere_intController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package desktopver;
+package controller;
 
 import entite.matiere;
 import java.net.URL;
