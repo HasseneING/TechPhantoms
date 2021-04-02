f0866abba2da3d198a0fd5d8240120c158c8a42f
ahlem <ahlem.laajili@esprit.tn>
2021-04-02 05:58:51 +0100

    update syrine

---
+++ FXDesktopApp/src/main/java/controllers/Mainone.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+public class Mainone extends Application {
+    public static boolean isSplashLoaded = false;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        URL root_url = new File("src/main/java/fxml/Student/Home.fxml").toURI().toURL();
+        Parent parent = FXMLLoader.load(root_url);
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.setTitle("TalkTo");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
