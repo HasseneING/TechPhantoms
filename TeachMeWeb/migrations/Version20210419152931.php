87dd8f85dcc854bb0263f53e8dabf2f076534165
Hassene <mohamedhassene.hamed@esprit.tn>
2021-04-19 18:01:19 +0100

    Update payment method Form

---
+++ TeachMeWeb/migrations/Version20210419152931.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210419152931 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE card ADD card_number VARCHAR(255) NOT NULL, ADD mm VARCHAR(2) NOT NULL, ADD yy VARCHAR(2) NOT NULL, ADD cvv VARCHAR(3) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE card DROP card_number, DROP mm, DROP yy, DROP cvv');
+    }
+}
