896337b735259bfa8c86203fa290de4a36bf3056
Hassene <mohamedhassene.hamed@esprit.tn>
2021-04-24 16:10:02 +0100

    Newsletter/mailer
    I have to pull the emails from the database and send them automatically

---
+++ TeachMeWeb/src/Controller/MailerController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/mailer", name="mailer")
+     */
+    public function index(): Response
+    {
+        return $this->render('mailer/index.html.twig', [
+            'controller_name' => 'MailerController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/email")
+     */
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new TemplatedEmail())
+            ->from('teachme242@gmail.com')
+            ->to('hamedhassenekun@gmail.com')
+            ->cc('teachme242@example.com')
+            ->subject('Time for Symfony Mailer!')
+            ->htmlTemplate('admin/datatable.html.twig');
+
+        $mailer->send($email);
+        return $this->render('mailer/index.html.twig', [
+            'controller_name' => 'MailerController',
+        ]);
+
+    }
+}
---
+++ TeachMeWeb/src/Controller/NewsletterSubsController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\NewsletterSubs;
+use App\Form\NewsletterSubsType;
+use App\Repository\NewsletterSubsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/newsletter/subs")
+ */
+class NewsletterSubsController extends AbstractController
+{
+    /**
+     * @Route("/", name="newsletter_subs_index", methods={"GET"})
+     */
+    public function index(NewsletterSubsRepository $newsletterSubsRepository): Response
+    {
+        return $this->render('newsletter_subs/index.html.twig', [
+            'newsletter_subs' => $newsletterSubsRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="newsletter_subs_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $newsletterSub = new NewsletterSubs();
+        $form = $this->createForm(NewsletterSubsType::class, $newsletterSub);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newsletterSub);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('base.html.twig', [
+            'newsletter_sub' => $newsletterSub,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="newsletter_subs_show", methods={"GET"})
+     */
+    public function show(NewsletterSubs $newsletterSub): Response
+    {
+        return $this->render('newsletter_subs/show.html.twig', [
+            'newsletter_sub' => $newsletterSub,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="newsletter_subs_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, NewsletterSubs $newsletterSub): Response
+    {
+        $form = $this->createForm(NewsletterSubsType::class, $newsletterSub);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('newsletter_subs_index');
+        }
+
+        return $this->render('newsletter_subs/edit.html.twig', [
+            'newsletter_sub' => $newsletterSub,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="newsletter_subs_delete", methods={"POST"})
+     */
+    public function delete(Request $request, NewsletterSubs $newsletterSub): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$newsletterSub->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($newsletterSub);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('newsletter_subs_index');
+    }
+}
